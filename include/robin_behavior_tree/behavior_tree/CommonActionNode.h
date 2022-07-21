96eeb96d6f51fddb812598ec9e7be98b49996be2
Nicola Conti <nicola@sysdesign.it>
2022-07-21 12:22:02 +0200

    Initial commit

---
+++ include/robin_behavior_tree/behavior_tree/CommonActionNode.h
@@ -0,0 +1,149 @@
+#ifndef ROBIN_BEHAVIOR_TREE_COMMON_ACTION_NODE_H
+#define ROBIN_BEHAVIOR_TREE_COMMON_ACTION_NODE_H
+// STL
+#include <string>
+#include <vector>
+// ROS
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <robin_bridge_generated/CommonCommand.h>
+#include <robin_bridge_generated/CommonFeedback.h>
+// BehaviorTree.CPP
+#include <behaviortree_cpp_v3/action_node.h>
+// Boost
+#include <boost/bind.hpp>
+
+
+namespace sysdesign { namespace bt {
+
+
+class CommonActionNode : public BT::StatefulActionNode {
+private:
+  const std::string LOGNAME = "CommonActionNode";
+
+  ros::NodeHandle node;
+
+  ros::Publisher command_pub;
+  ros::Subscriber feedback_sub;
+
+  BT::NodeStatus status;
+
+  // command
+  bool execute;
+  bool abort;
+
+  // feedback
+  bool busy;
+  bool done;
+  bool error;
+  bool aborted;
+  int error_id;
+
+public:
+
+  CommonActionNode(const std::string &name, const BT::NodeConfiguration &config)
+  : BT::StatefulActionNode(name, config),
+    node("/robin")
+  {
+    command_pub = node.advertise<robin_bridge_generated::CommonCommand>("command", 10, true);
+    feedback_sub = node.subscribe<robin_bridge_generated::CommonFeedback>("feedback", 10, boost::bind(&CommonActionNode::feedback_cb, this, _1));
+  }
+
+  void feedback_cb(const robin_bridge_generated::CommonFeedback::ConstPtr &msg)
+  {
+    busy = msg->busy;
+    done = msg->done;
+    error = msg->error;
+    aborted = msg->aborted;
+    error_id = msg->error_id;
+  }
+
+  static BT::PortsList providedPorts()
+  {
+    return
+    {
+      BT::InputPort<int>("command_id"),
+      BT::OutputPort<int>("error_id"),
+    };
+  }
+
+  BT::NodeStatus onStart() override
+  {
+    execute = true;
+    abort = false;
+
+    robin_bridge_generated::CommonCommand msg;
+    msg.execute = execute;
+    msg.abort = abort;
+    msg.id = getInput<int>("command_id").value();
+    command_pub.publish(msg);
+
+    return BT::NodeStatus::RUNNING;
+  }
+
+  BT::NodeStatus onRunning() override
+  {
+    if (done)
+    {
+      execute = false;
+      abort = false;
+
+      robin_bridge_generated::CommonCommand msg;
+      msg.execute = execute;
+      msg.abort = abort;
+      msg.id = getInput<int>("command_id").value();
+      command_pub.publish(msg);
+
+      status = BT::NodeStatus::SUCCESS;
+    }
+    if (error)
+    {
+      execute = false;
+      abort = false;
+
+      robin_bridge_generated::CommonCommand msg;
+      msg.execute = execute;
+      msg.abort = abort;
+      msg.id = getInput<int>("command_id").value();
+      command_pub.publish(msg);
+
+      status = BT::NodeStatus::FAILURE;
+    }
+    if (aborted)
+    {
+      execute = false;
+      abort = false;
+
+      robin_bridge_generated::CommonCommand msg;
+      msg.execute = execute;
+      msg.abort = abort;
+      msg.id = getInput<int>("command_id").value();
+      command_pub.publish(msg);
+
+      status = BT::NodeStatus::FAILURE;
+    }
+
+    if (!execute && !busy && !done && !error && !aborted)
+    {
+      return status;
+    }
+
+    return BT::NodeStatus::RUNNING;
+  }
+
+  void onHalted() override
+  {
+    execute = false;
+    abort = true;
+
+    robin_bridge_generated::CommonCommand msg;
+    msg.execute = execute;
+    msg.abort = abort;
+    msg.id = getInput<int>("command_id").value();
+    command_pub.publish(msg);
+  }
+
+};
+
+} }  // namespace
+#endif
---
+++ src/behavior_tree_node.cpp
@@ -0,0 +1,59 @@
+#include <string>
+// ROS
+#include <ros/ros.h>
+#include <ros/console.h>
+// BehaviorTree.CPP
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
+#include <behaviortree_cpp_v3/loggers/bt_file_logger.h>
+#include <behaviortree_cpp_v3/loggers/bt_zmq_publisher.h>
+//
+#include <robin_behavior_tree//behavior_tree/CommonActionNode.h>
+
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "behavior_tree_node");
+
+  // Node
+  ros::NodeHandle node("~");
+
+  // Parameters
+  double freq = node.param("frequency", 1.0);
+
+  std::string tree_path;
+  if (!node.getParam("tree_path", tree_path))
+  {
+    std::string param_name = node.resolveName("tree_path");
+    ROS_FATAL("Failed to retrieve '%s' parameter.", param_name.c_str());
+    return 1;
+  }
+
+  /* BEHAVIOR TREE */
+  BT::BehaviorTreeFactory bt_factory;
+  bt_factory.registerNodeType<sysdesign::bt::CommonActionNode>("CommonAction");
+
+  auto tree = bt_factory.createTreeFromFile(tree_path);
+
+  /* LOGGER */
+  BT::StdCoutLogger logger(tree);
+
+  /* FILE LOGGER */
+  // BT::FileLogger file_logger(tree, "bt_trace.fbl");
+
+  /* ROSOUT LOGGER */
+  // BT::RosoutLogger rosout_logger(tree.rootNode());
+
+  /* GROOT */
+  BT::PublisherZMQ publisher_zmq(tree);
+
+  // Loop
+  ros::Rate rate(freq);
+  while (ros::ok() && (tree.tickRoot() != BT::NodeStatus::SUCCESS))
+  {
+    rate.sleep();
+    ros::spinOnce();
+  }
+
+  return 0;
+}
